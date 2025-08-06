8d3650b8eb4e61a234c7cf1e449f6561c106a9c7
pallavikishor <kishorpallavi2@gmail.com>
2025-08-06 20:18:16 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3)
+
+// code readibility should be high
+
+console.log("pallavi")
+
+let name = "Pallavi"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+
+// PRIMITIVE DATATYPE
+
+// number =>  2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => define unique
+
+//object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object

da540dbab906710646699da792df50e340203f94
pallavikishor <kishorpallavi2@gmail.com>
2025-08-07 21:27:11 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,21 +1,21 @@
-//let score = "33abc"
-//let score = null
-//let score = undefined
-//let score = true
-let score = "Pallavi"
-//console.log(typeof score);
-//console.log(typeof(score));
+/*let score = "33abc"
+let score = null
+let score = undefined
+let score = true
+*/let score = "Pallavi"
+console.log(typeof score);
+console.log(typeof(score));
 
 let valueInNumber = Number(score)
-//console.log(typeof(valueInNumber));
-//console.log(valueInNumber)// NaN  //0 //NaN //1 //NaN
+console.log(typeof(valueInNumber));
+console.log(valueInNumber)// NaN  //0 //NaN //1 //NaN
 
-//let isLoggedIn = 1
-//let isLoggedIn = 0
-//let isLoggedIn = ""
-//let isLoggedIn = "Pallavi"
-//let booleanIsLoggedIn = Boolean(isLoggedIn)
-//console.log(booleanIsLoggedIn);//true   //false  // false //true
+/*let isLoggedIn = 1
+let isLoggedIn = 0
+let isLoggedIn = ""
+*/let isLoggedIn = "Pallavi"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);//true   //false  // false //true
 
 let someNumber = 27
 let stringNumber = String(someNumber)

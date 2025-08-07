99e92e10db1007db65fe0bde191e6e8389640879
pallavikishor <kishorpallavi2@gmail.com>
2025-08-07 20:49:29 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+//let score = "33abc"
+//let score = null
+//let score = undefined
+//let score = true
+let score = "Pallavi"
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof(valueInNumber));
+//console.log(valueInNumber)// NaN  //0 //NaN //1 //NaN
+
+//let isLoggedIn = 1
+//let isLoggedIn = 0
+//let isLoggedIn = ""
+//let isLoggedIn = "Pallavi"
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);//true   //false  // false //true
+
+let someNumber = 27
+let stringNumber = String(someNumber)
+console.log(stringNumber);//27
+console.log(typeof stringNumber);//String
+
+

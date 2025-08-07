051dd474320a79b564dfa85aa190503e4eb9c262
pallavikishor <kishorpallavi2@gmail.com>
2025-08-07 21:26:44 +0000

    operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,40 @@
+// OPERATIONS
+
+let value = 3
+let negValue = - value
+console.log(negValue);//-3
+
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 ** 3);
+console.log(2 / 2);
+console.log(2 % 2);
+
+
+let str1 = "hello"
+let str2 = " Pallavi"
+let str3 = str1 + str2;
+console.log(str3);//hello Pallavi
+
+
+console.log("1" + 2);//12
+console.log(1 + "2");//12
+console.log("1" + "2");//12
+console.log("1" + 2 + 2);//122
+console.log(1 + 2 + "2");//32
+console.log((3 + 4) * 5 % 3);//2
+
+console.log(true); // true
+console.log(+true);//1
+console.log(-true);//-1
+console.log(+"");//0
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);//101 // 101

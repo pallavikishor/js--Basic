9007954c6f2acd01cde0dc2339bf64066e2d75d8
pallavikishor <kishorpallavi2@gmail.com>
2025-08-12 20:45:25 +0000

    array

--- 02_basics/01_Array.js
+++ 02_basics/01_Array.js
@@ -3,26 +3,26 @@
 const myArr = [0, 1, 2, 3, 4, 5]
 const heros = ["palavi", "devika"]
 const myArr2 = new Array(1, 2, 3, 4)
-//console.log(myArr[2]);
+console.log(myArr[2]);
 
 
-// Array methods
+ Array methods
 
 
-//myArr.push(6)
-//myArr.push(7)
-//myArr.pop()
+myArr.push(6)
+myArr.push(7)
+myArr.pop()
 
-//myArr.unshift(9)
-//myArr.shift()
+myArr.unshift(9)
+myArr.shift()
 
-/*console.log(myArr.includes(9));
+console.log(myArr.includes(9));
 console.log(myArr.indexOf(3));
 
 const newArr = myArr.join()
 console.log(myArr);
 console.log(typeof newArr);
-*/
+
 
 // Slice, splice
 
